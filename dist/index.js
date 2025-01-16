948e6a1d1e38849b50a66c85b05366f310b6ad60
patrick brisbin <pbrisbin@gmail.com>
2025-01-16 08:31:41 -0500

    Build

--- dist/index.js
+++ dist/index.js
@@ -267,15 +267,15 @@ class GenHIE {
         }
     }
     async generate() {
-        if (this.exists && this.installed) {
-            const contents = await this.stack.read([
-                "exec",
-                "--",
-                "gen-hie",
-                "--stack",
-            ]);
-            fs.writeFileSync(this.path, contents);
+        if (!this.exists) {
+            core.info(`Skipping, ${this.path} does not exist`);
+            return;
         }
+        if (!this.installed) {
+            core.info(`Skipping, implicit-hie was not successfully installed`);
+        }
+        const yaml = await this.stack.read(["exec", "--", "gen-hie", "--stack"]);
+        fs.writeFileSync(this.path, yaml);
     }
 }
 exports.GenHIE = GenHIE;
